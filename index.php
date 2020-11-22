33616f034f5d0d2c57734d5c4a2c52063bb50e9d
psychohaxer <apclead69@gmail.com>
2020-11-22 17:43:42 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title> Aplikasi Forecasting </title>
+
+        <link rel="stylesheet" type="text/css" href="css/screen.css">
+        <link rel="stylesheet" type="text/css" href="css/object.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        
+        <style ="text/css">
+            body {
+                background-image:url("img/haihead.jpg");
+                background-repeat:no-repeat;
+                background-position:center;
+                background-attachment:fixed;
+            }
+        </style>
+        
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+        
+        <table border="0px" width="100%" id="kop">
+            <tr id=kop>
+                <td width="10%"><img src="img/Logo1.png" height="120px" width="120px" border="10px" id="kop"> </td>
+                <td width="80%" id="judul"> <center> Aplikasi Forecasting </center> </td>
+                <td width="10%"><img src="img/Logo2.png" height="120px" width="120px" border="10px" id="kop"> </td>
+            </tr>
+        </table>
+        
+        <br>
+
+        <nav id="header">
+            <ul id="navbar">
+                <a href="#"> <li>✍ Input Data </li> </a>
+                <a href="#"> <li>✨ Prediksi Data </li> </a>
+            </ul>
+        </nav>
+
+    </body>
+</html>
