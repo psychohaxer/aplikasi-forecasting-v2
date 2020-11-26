94f4a8fcacfffbab9c53b3c4490eb775340b909c
psychohaxer <apclead69@gmail.com>
2020-11-26 12:18:27 +0700

    Separating header and footer

---
+++ header.php
@@ -0,0 +1,18 @@
+<table border="0px" width="100%" id="kop">
+    <tr id=kop>
+        <td width="10%"><img src="img/Logo1.png" height="120px" width="120px" border="10px" id="kop"> </td>
+        <td width="80%" id="judul"> <h1 id="judul"><center> Aplikasi Forecasting </center></h1> </td>
+        <td width="10%"><img src="img/Logo2.png" height="120px" width="120px" border="10px" id="kop"> </td>
+    </tr>
+</table>
+
+<br>
+
+<nav id="header">
+    <ul id="navbar">
+        <a href="lihat.php"> <li>✍ CRUD Data </li> </a>
+        <a href="perkiraan.php"> <li>✨ Prediksi Data </li> </a>
+    </ul>
+</nav>
+
+<br>
