383938d6c3de9199aefe202894502a42207bec74
psychohaxer <apclead69@gmail.com>
2020-11-28 23:28:59 +0700

    Working on forecasting

--- perkiraan.php
+++ perkiraan.php
@@ -40,20 +40,52 @@
                 </tr>
                 <?php
                     $result=$forecast->lihatData();
-                    $x=0;
+                    $x = $sx = $sy = $sxx = $sxy=0;
                     while ($row=$result->fetch_assoc()){
                 ?>
                 <tr id="ramalan">
-                    <td id="ramalan"><aaa><?php echo "Minggu ke-" , $row['minggu'] , " Bulan " , $row['bulan'] , " Tahun " , $row['tahun'];?></aaa></td>
+                    <td id="ramalan"><aaa><?php echo "Minggu ke-" , $row['minggu'] , " Bulan " , $row['bulan'] , "<br>Tahun " , $row['tahun'];?></aaa></td>
                     <td id="ramalan"><aaa><?php echo $row['jumlah'];?></aaa></td>
                     <td id="ramalan"><aaa><?php echo $x;?></aaa></td>
                     <td id="ramalan"><aaa><?php echo $row['jumlah'];?></aaa></td>
                     <td id="ramalan"><aaa><?php echo $x*$x;?></aaa></td>
-                    <td id="ramalan"><aaa><?php echo $row['jumlah']*$x; $x++?></aaa></td>
+                    <td id="ramalan"><aaa><?php echo $row['jumlah']
+                    *$x;
+                    $sx+=$x;
+                    $sy+=$row['jumlah'];
+                    $sxx+=($x*$x);
+                    $sxy+=($row['jumlah']*$x);
+                    $x++;?></aaa></td>
                 </tr>
                 <?php
                     }
                 ?>
+                
+            </table>
+            <table id="data">
+                <tr>
+                    <th id="judul" colspan="2"> Jumlah </th>
+                    <th id="judul"> <?php echo $sx ?></th>
+                    <th id="judul"> <?php echo $sy ?></th>
+                    <th id="judul"> <?php echo $sxx ?></th>
+                    <th id="judul"> <?php echo $sxy ?></th>
+                </tr>
+                <tr>
+                    <th id="judul" colspan="2"> Rata-rata </th>
+                    <th id="judul"> <?php echo ($sx/$x) ?></th>
+                    <th id="judul"> <?php echo ($sy/$x) ?></th>
+                    <th id="judul"> - </th>
+                    <th id="judul"> - </th>
+                </tr>
+                <?php
+                    $no = $x+1;
+                    $b1 = ($sxy - (($sx * $sy)/$no))/($sxx-(($sx*$sx)/$no));
+                    $b0 = ($sy/$no) - ($b1*($sx/$no));
+                ?>
+                <tr>
+                    <th id="judul" colspan="2"> Rumus Regresi Linear: </th>
+                    <th id="judul" colspan="4"> <?php echo "$b0 + $b1 x"; ?> </th>
+                </tr>
             </table>
         </div>
 
