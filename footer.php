3d5aefa8df50d5164f10179f603ed4784b0a3b3e
Firman Adi Nur Fatin <apclead69@gmail.com>
2023-05-13 00:10:46 +0700

    edit footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
         <div id="about" class="d-flex justify-content-center">
-            <p class="text-center"> Firman Adi Nur Fatin | <a class="text-reset" href="https://github.com/psychohaxer/aplikasi-forecasting-v2">psychohaxer/aplikasi-forecasting-v2</a></p>
+            <p class="text-center"> Created by Firman Adi Nur Fatin | <a class="text-reset" href="https://github.com/psychohaxer/aplikasi-forecasting-v2">psychohaxer/aplikasi-forecasting-v2</a></p>
         </div>
         <script src="js/bootstrap.js"></script>
     </body>
